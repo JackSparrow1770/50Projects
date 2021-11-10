df215de944065a052a4e7759daeb78f2c191550c
Fahad Siddique <86157508+JackSparrow1770@users.noreply.github.com>
2021-11-10 16:29:42 +0600

    third project is completed. all understood.

---
+++ project3_Rotating_navigation/script.js
@@ -0,0 +1,12 @@
+const opentag = document.getElementById('open');
+const closetag = document.getElementById('close');
+const container = document.querySelector('.container');
+
+opentag.addEventListener('click', () => {
+    container.classList.add('show-nav')
+})
+
+closetag.addEventListener('click', () => {
+    container.classList.remove('show-nav')
+})
+
